9471ffbfc7c331af5536396d2d6332c78a3c7d5a
Anderson Lozano <adslozanov@gmail.com>
2025-02-20 15:54:37 -0500

    Project created 🎉

---
+++ src/main/java/com/technical/test/technicaltestsivargroup/TechnicalTestSivarGroupApplication.java
@@ -0,0 +1,13 @@
+package com.technical.test.technicaltestsivargroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestSivarGroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalTestSivarGroupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/technical/test/technicaltestsivargroup/TechnicalTestSivarGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical.test.technicaltestsivargroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestSivarGroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
